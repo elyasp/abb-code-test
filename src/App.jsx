b5b255b54d4e99bf53a39b7fff56cf08502285a7
Elyas <Essassani>
2020-02-23 21:50:21 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p>Welcome to Machine-Interface</p>
+    </div>
+  );
+}
+
+export default App;
